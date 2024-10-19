ea5bbd6d0c39203da738e3cc763358127705b50a
KostyaMaksimov <maksimovkonstantin44@gmail.com>
2024-10-19 12:51:58 +0300

    task1a

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Форум</title>
+</head>
+<body>
+    <div>
+    <h1>Посты</h1>
+</div>
+    <div>
+        <h1>Оставить сообщение</h1>
+</div>
+</body>
+</html>
